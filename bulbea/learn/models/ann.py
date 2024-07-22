5329eb5b4e4e74251ab4cc7fdc1c0577e240c98f
Data scientist <achraf.louiza97@gmail.com>
2024-07-23 01:39:46 +0200

    Update ann.py

--- bulbea/learn/models/ann.py
+++ bulbea/learn/models/ann.py
@@ -2,16 +2,16 @@ from __future__ import absolute_import
 from six import with_metaclass
 
 from keras.models import Sequential
-from keras.layers import LSTM, GRU, Dense, Activation, Dropout
+from keras.layers import LSTM, GRU, SimpleRNN, Dense, Activation, Dropout
 from bulbea.learn.models import Supervised
 
 class ANN(Supervised):
     pass
 
 class RNNCell(object):
-    RNN  = recurrent.SimpleRNN
-    GRU  = recurrent.GRU
-    LSTM = recurrent.LSTM
+    RNN  = SimpleRNN
+    GRU  = GRU
+    LSTM = LSTM
 
 class RNN(ANN):
     def __init__(self, sizes,
@@ -22,19 +22,19 @@ class RNN(ANN):
                  optimizer  = 'rmsprop'):
         self.model = Sequential()
         self.model.add(cell(
-            input_dim        = sizes[0],
-            output_dim       = sizes[1],
+            units           = sizes[1],  # 'units' instead of 'output_dim'
+            input_shape     = (None, sizes[0]),  # input_shape should be (timesteps, features)
             return_sequences = True
         ))
 
         for i in range(2, len(sizes) - 1):
-            self.model.add(cell(sizes[i], return_sequences = False))
+            self.model.add(cell(units=sizes[i], return_sequences=True))
             self.model.add(Dropout(dropout))
 
-        self.model.add(Dense(output_dim = sizes[-1]))
+        self.model.add(Dense(units=sizes[-1]))  # 'units' instead of 'output_dim'
         self.model.add(Activation(activation))
 
-        self.model.compile(loss = loss, optimizer = optimizer)
+        self.model.compile(loss=loss, optimizer=optimizer)
 
     def fit(self, X, y, *args, **kwargs):
         return self.model.fit(X, y, *args, **kwargs)
